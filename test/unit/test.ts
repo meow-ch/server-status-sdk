2a9dc9e7dc7b7fb0f30d35f2b41a869f1a711a06
Guillermo Pages <mail@guillermo.at>
2021-12-03 00:45:17 +0200

    fix: remove build old dirs

---
+++ test/unit/test.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('test', function () {
+  describe(`some test`, function() {
+    it('should make 0 equal 0', async function() {
+      expect(0).to.equal(0);
+    });
+  });
+});
+
