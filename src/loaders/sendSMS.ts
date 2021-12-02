9f344a240e5eb9192e53923da09dd32c39038b38
Guillermo Pages <mail@guillermo.at>
2021-12-02 11:53:35 +0200

    fix: missing logger in sendSMS

--- src/loaders/sendSMS.ts
+++ src/loaders/sendSMS.ts
@@ -1,19 +1,25 @@
 
 import { LoadDictElement } from 'di-why/build/src/DiContainer';
+import Logger from 'saylo';
 import sendSMS from '../utils/sendSMS';
 
+type SendSMSFactoryProps = { apiKey?: string; apiSecret?: string; toCommaList?: string; logger: Logger; };
+
 const loadDictElement: LoadDictElement<(from: string, text: string) => Promise<string[]>> = {
-  factory: function ({ apiKey, apiSecret, toCommaList }: { apiKey?: string; apiSecret?: string; toCommaList?: string; }) {
+  factory: function ({ apiKey, apiSecret, toCommaList, logger}: SendSMSFactoryProps) {
     if (!toCommaList || toCommaList.length <= 3 || !apiKey || !apiSecret) {
       return async (_: string, __: string) => ['No phone number | apiKey | apiSecret provided, skipping SMS sending.'];
     }
-    return sendSMS(apiKey, apiSecret, toCommaList.split(','));
+    return sendSMS(apiKey, apiSecret, toCommaList.split(','), logger);
   },
   deps: {
     toCommaList: process.env.SMS_TO,
     apiKey: process.env.SMS_NEXMO_API_KEY,
     apiSecret: process.env.SMS_NEXMO_API_SECRET,
   },
+  locateDeps: {
+    logger: 'logger',
+  }
 };
 
 export default loadDictElement;
